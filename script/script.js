2c4a91d94a62d6b2df625b9c12636501e151f0e8
Hadisehzamani <hadisehzmn@gmail.com>
2023-08-01 12:48:47 +0330

    edit transition

--- script/script.js
+++ script/script.js
@@ -6,15 +6,7 @@ btn.forEach(function(item){
     item.addEventListener('click', function(){
         let panel = this.nextElementSibling;
         let newI = item.querySelector('i')
-        if(panel.style.display === 'none'){
-            panel.style.display = 'block'
-            newI.classList.remove('fa-solid', 'fa-plus')
-            newI.classList.add('fa-solid', 'fa-minus')
-        }else {
-            panel.style.display = 'none'
-            newI.classList.remove('fa-solid', 'fa-minus')
-            newI.classList.add('fa-solid', 'fa-plus' )
-        }
+        panel.classList.toggle('display-block');
 
     })
 });
